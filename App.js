23084495ff33ccf7ef42e58cd5c75a03f85f02df
esighu <esighu@gmail.com>
2022-07-04 20:16:00 -0300

    Clase 3

--- App.js
+++ App.js
@@ -1,22 +1,47 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hola Mundo Coder!!!!</Text>
-      <Text>Estoy aqui, mi nombre es Esteban Hurtado</Text>
-      <StatusBar style="auto" />
+    <View style={ styles.screen }>
+      <View style ={styles.inpuntContainer}>
+        <TextInput placeholder='Ingrese aqui' style={styles.inpunt} />
+        <Button title='Agregar' onPress={() => {}} />
+      </View>
+      <View>
+          <View style ={{padding: 20}}>
+            <Text  style={styles.inpunt} >hola </Text>
+          </View>
+          <View style ={styles.inpuntContainer}>
+            <TextInput placeholder='Ingrese aqui' style={styles.inpunt} />
+          </View>
+          <View style ={styles.inpuntContainer}>
+            <TextInput placeholder='Ingrese aqui' style={styles.inpunt} />
+          </View>
+          <View style ={styles.inpuntContainer}>
+            <TextInput placeholder='Ingrese aqui' style={styles.inpunt} />
+          </View>
+      </View>  
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen:{
+    padding: 30,
   },
+  inpuntContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    },
+  inpunt: {
+    borderBottomColor: 'black',
+    borderBottomWidth: 1, 
+    width: '80%', 
+    height: 50,
+  },
+  
 });
+
